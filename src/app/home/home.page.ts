34de1acb10d4fd0d32a5b6217ce2727a6bcab96d
Fauzi Halabe <fauzihalabe@gmail.com>
2020-10-30 10:22:43 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  mensagens = [];
+  mensagem: string = "";
+  baseApi = 'http://localhost:8080/';
+  sessao = null;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    //Capturar token de sessão
+    this.http.post(this.baseApi + 'createSession', {}, {})
+      .subscribe((r) => {
+        console.log('Sessão response', r);
+        this.sessao = r['session'];
+      })
+  }
+
+  enviar() {
+    if (this.mensagem != '') {
+      console.log('enviar mensagem');
+
+      //Gravar mensagem do usuario
+      this.mensagens.push({ texto: this.mensagem, de: 'user' });
+
+      //Enviar para o bot e obter uma resposta
+      this.http.post(this.baseApi + 'newMessage', { session: this.sessao, text: this.mensagem }, {})
+        .subscribe((r) => {
+          console.log('Mensagem response', r);
+          //Gravar mensagem do bot
+          this.mensagens.push({ texto: r['response'], de: 'bot' });
+
+          //Limpar caixa de texto
+          this.mensagem = "";
+        })
+    }
+  }
 
 }
